0fa16b1c25000e4509926926389a73f70df6a1c0
waynehuang <waynehuang119@hotmail.com>
2024-11-28 10:17:14 -0600

    fixed point calculation. All features completed

--- services/receipts_service.go
+++ services/receipts_service.go
@@ -69,14 +69,13 @@ func GetPoints(id string) (int64, error) {
 func calculatePoints(receipt model.Receipt) int64 {
 	var points int64
 
-	// Rule 1: 1 point for every alphanumeric character in the retailer name.
+	// Rule 1: One point for every alphanumeric character in the retailer name.
 	retailerRegex := regexp.MustCompile(`[a-zA-Z0-9]`)
 	points += int64(len(retailerRegex.FindAllString(receipt.Retailer, -1)))
 
-	// Rule 2: 50 points if the total is a round dollar amount.
+	// Rule 2: 50 points if the total is a round dollar amount with no cents.
 	total, err := strconv.ParseFloat(receipt.Total, 64)
 	if err != nil {
-		// Handle error, e.g., return 0 points if total is invalid
 		return points
 	}
 	if total == float64(int(total)) {
@@ -91,13 +90,13 @@ func calculatePoints(receipt model.Receipt) int64 {
 	// Rule 4: 5 points for every two items on the receipt.
 	points += int64(len(receipt.Items) / 2 * 5)
 
-	// Rule 5: If the trimmed description of an item has a length divisible by 3, multiply the price by 0.2 and round up.
+	// Rule 5: If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
 	for _, item := range receipt.Items {
 		trimmedDescription := strings.TrimSpace(item.ShortDescription)
 		if len(trimmedDescription)%3 == 0 {
 			price, err := strconv.ParseFloat(item.Price, 64)
 			if err == nil {
-				points += int64((price * 0.2) + 0.5) // Round up
+				points += int64(math.Ceil(price * 0.2))
 			}
 		}
 	}
@@ -108,7 +107,7 @@ func calculatePoints(receipt model.Receipt) int64 {
 		points += 6
 	}
 
-	// Rule 7: 10 points if the purchase time is between 2:00 PM and 4:00 PM.
+	// Rule 7: 10 points if the time of purchase is after 2:00pm and before 4:00pm.
 	hour, err := strconv.Atoi(strings.Split(receipt.PurchaseTime, ":")[0])
 	if err == nil && hour >= 14 && hour < 16 {
 		points += 10
